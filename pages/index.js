27fb5a6a4d360875cf3282f16378f550156e6659
Gaurav Dhiman <gauravd.chd@gmail.com>
2020-05-07 23:44:40 -0700

    Removed the semi-colon in google-analytics script that was resulting build issue.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = () => (
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6297967-3"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
 
         gtag('config', 'UA-6297967-3');
